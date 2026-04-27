53166bfdc4bd09161ca2a6dc4166d74eba82efe4
tonyrayf <wintryshades@gmail.com>
2026-04-27 23:15:59 +0700

    minor changes

--- scenes/card.gd
+++ scenes/card.gd
@@ -1,18 +1,26 @@
 extends Area2D
 
 
-var dragged : bool = false
+var entered : bool = false
+var dragging : bool = false
 
 
 func _process(delta: float) -> void:
-	if dragged:
+	if entered:
+		if Input.is_action_pressed("mouse_lb"):
+			dragging = true
+		else:
+			dragging = false
+	
+	if dragging:
 		global_position = Global.cursor.global_position
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area == Global.cursor and Input.is_action_pressed("mouse_lb"):
-		dragged = true
+	if area == Global.cursor:
+		entered = true
 
 func _on_area_exited(area: Area2D) -> void:
 	if area == Global.cursor:
-		dragged = false
+		entered = false
+		dragging = false
