5bc1562a5c6399c5b7f98ec1b8fc5b748adacd1e
tonyrayf <wintryshades@gmail.com>
2026-04-28 23:32:25 +0700

    added fallens sprites and cool shader for them

---
+++ scripts/fallen_visuals.gd
@@ -0,0 +1,10 @@
+extends Sprite3D
+
+
+func change_fallen_sprite(tex: Texture):
+	texture = tex
+	material_override.set_shader_parameter("albedo_texture", tex)
+
+
+func _ready() -> void:
+	change_fallen_sprite(texture)
