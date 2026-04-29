6eadc468e746657cb1677d59d44b927d21b98005
Alexey <nevmmr@gmail.com>
2026-04-29 18:28:01 +0700

    pre fetch commit

--- scripts/main_logic.gd
+++ scripts/main_logic.gd
@@ -32,8 +32,11 @@ class card:#класс карты
 		
 		match entityToGet:
 			Entities.PLAYER:
-				card_node.global_position = Vector3(0, 0, 0)
-			
+				card_node.scale = Vector3(1.45,1.45,1.45)
+				if MainLogic.player_cards.size()==1:
+					card_node.global_position = Vector3(-2.480099, 1.023664, 0.882562)
+				elif MainLogic.player_cards.size()==2:
+					card_node.global_position = Vector3(-1.389761, 1.023664, 0.73708)
 			Entities.SHARED:
 				card_node.global_position = Vector3(0, 0, -3)
 			
