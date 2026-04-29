41b9d5dbaf4aeeff63f628671f17a12de915aefa
Alexey <nevmmr@gmail.com>
2026-04-29 09:47:44 +0700

    added base arcane effect, changed fold/not fold to work with signals, and some minor changes

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+signal doFold(flag: bool)
 
 var cursor : Node
 var mainScene: Node3D
@@ -8,6 +9,7 @@ var cardsContainer: Node3D
 var current_day : int = 1
 var playerHP: float = 100.0
 var enemyHP: float = 150.0
+var damageMultiplier: float = 1
 
 func play_or_fold() -> bool:#вызывается, ждёт от игрока продолжения игры или сброса карт
 	var result = ""
@@ -18,3 +20,11 @@ func play_or_fold() -> bool:#вызывается, ждёт от игрока п
 		result = false
 	await get_tree().create_timer(0.5).timeout
 	return result
+	
+func _input(event):
+	# Проверяем, что нажата клавиша и что это событие "нажатия" (а не отпускания)
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_A:
+			doFold.emit(false)
+		elif event.keycode == KEY_D: 
+			doFold.emit(true)
