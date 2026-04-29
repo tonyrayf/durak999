51908816414ff233cb02c339edeb1a4d89e3ab29
tonyrayf <wintryshades@gmail.com>
2026-04-29 18:26:38 +0700

    for oleksiy

--- scenes/button_play.gd
+++ scenes/button_play.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		Global.doFold.emit()
+		Global.doFold.emit(false)
